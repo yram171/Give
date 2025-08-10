c5ae6541efc6ca567bbfdbaea6abfbf8cf2d2564
jaydelal13 <jlal056@aucklanduni.ac.nz>
2025-08-10 23:44:10 +1200

    Implemented basic frontend design for the login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/Login.css';
 
 const Login = () => {
-
   return (
     <div className="login-container">
+      <div className="left-side">
+        <div className="title-container">
+          <header className="give-title">
+            <h1>Give</h1>
+          </header>
+          <p className="slogan">Can't decide? Let them vote.</p>
+        </div>
+      </div>
 
+      <div className="right-side">
+        <div className="login-box">
+          <input
+            type="text"
+            placeholder="Email or Phone Number"
+            className="login-input"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="login-input"
+          />
+          <button className="login-btn">Log In</button>
+          <button className="forgot-btn">Forgot Account?</button>
+          <button className="create-btn">Create Account</button>
+        </div>
+      </div>
     </div>
   );
 };
