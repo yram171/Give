a880fafbc2382bf22fc0aa7c5af4835c4e0a29c2
jaydelal13 <jlal056@aucklanduni.ac.nz>
2025-08-10 22:42:50 +1200

    added basic components for the login page, plus routing

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import '../styles/Login.css';
+
+const Login = () => {
+
+  return (
+    <div className="login-container">
+
+    </div>
+  );
+};
+
+export default Login;
