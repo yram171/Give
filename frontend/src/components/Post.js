d8e6c1ad97725a64059fe76b542ae81ccd8567e0
AnnaChibel <160548460+AnnaChibel@users.noreply.github.com>
2025-08-12 09:38:22 +1200

    Added colours to the tialwind config file for easy use

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -10,5 +10,10 @@ export default function Post({
   profilePic,
   pollOptions
 }) {
-  return;
+  return (
+    <div className="bg-[#F5F3F3] p-3 rounded-lg max-w-md mx-auto">
+      <div className="bg-white rounded-xl shadow p-4"></div>
+     
+      </div>
+  );
 }
