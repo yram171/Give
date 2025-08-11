364e7aff96c7a24c242f838787aeec304623d1c8
AnnaChibel <160548460+AnnaChibel@users.noreply.github.com>
2025-08-12 09:23:59 +1200

    Added the function parameter list

---
+++ frontend/src/components/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PollBox from "./PollBox";
+
+export default function Post({
+  username,
+  groupName,
+  timeLeft,
+  question,
+  images,
+  profilePic,
+  pollOptions
+}) {
+  return;
+}
