57fe6bcccd5ebfa25aae1b2f8e4a4072c6a60fc8
AnnaChibel <160548460+AnnaChibel@users.noreply.github.com>
2025-08-17 22:53:40 +1200

    Added a link reference to the create and explore pages

--- frontend/src/components/ScreenTab/ScreenTab.js
+++ frontend/src/components/ScreenTab/ScreenTab.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function ScreenTab() {
 
@@ -19,31 +20,29 @@ export default function ScreenTab() {
               <p className="font-semibold text-base text-black ">Home Feed</p>
             </div>
         </div>
-        <div className="flex items-center p-2 rounded-xl  bg-backgroundGrey gap-3 hover:bg-lightYellow">
-              <div>
+        <Link
+            to="/explore"
+            className="flex items-center p-2 rounded-xl bg-backgroundGrey gap-3 hover:bg-lightYellow"
+            >
                 <img
                   src="/Eye.png"
                   alt="eye icon"
                   style={{ width: "1.5em", height: "1.5em" }}
                 />
-              </div>
-              <div>
                 <p className="font-semibold text-base text-black">Explore Polls</p>
-              </div>
-        </div>
-        <div className="flex items-center p-2 rounded-xl  bg-backgroundGrey gap-3 hover:bg-lightYellow">
-          <div>
+        </Link>
+        <Link
+            to="/create"
+            className="flex items-center p-2 rounded-xl bg-backgroundGrey gap-3 hover:bg-lightYellow"
+          >
               <img
                 src="/Plus.png"
                 alt="plus icon"
                 style={{ width: "1.5em", height: "1.5em" }}
               />
-            </div>
-            <div>
               <p className="font-semibold text-base text-black">Create Post</p>
-            </div>
-          </div>
-        </div>
+        </Link>
       </div>
+    </div>
   );
 }
