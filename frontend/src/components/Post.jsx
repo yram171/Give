4c51cbd6a1b9233bf177a35d81a8d3809de2a6a9
Aye <160553101+ayepricots@users.noreply.github.com>
2025-08-17 17:45:30 +1200

    Translated all css files to tailwind css

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -3,11 +3,9 @@ import UserCard from "./UserCard";
 import PostData from "./PostData";
 import PollBox from "./PollBox";
 
-import './Post.css';
-
 const Post = ({user, group, post, pollOptions}) => {
   return (
-    <div className='root'>
+    <div className="flex flex-col bg-backgroundGrey gap-4 p-6 rounded-xl w-full max-w-2xl mx-auto">
       <UserCard user={user} group={group} timeLeft={post.timeLeft} />
       <PostData post={post} />
       <PollBox initialOptions={pollOptions} />
